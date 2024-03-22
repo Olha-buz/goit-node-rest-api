a40cfcb9a4b302a73e183b3310cf3e62bda72e3c
Olha-buz <134137848+Olha-buz@users.noreply.github.com>
2024-03-22 14:42:01 +0200

    fetch/verification

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { current, login, logout, register, uploadAvatar } from "../controllers/authControlles.js";
+import { current, login, logout, register, resendVerifyEmail, uploadAvatar, verifyEmail } from "../controllers/authControlles.js";
 import { authentificate } from "../middlewares/authentificate.js";
 import { upload } from "../middlewares/upload.js";
 import validateBody from "../helpers/validateBody.js";
-import { loginSchema, registerSchema } from "../schemas/userSchemas.js";
+import { emailSchema, loginSchema, registerSchema } from "../schemas/userSchemas.js";
 
 const userRouter = express.Router();
 const jsonParser = express.json();
@@ -13,5 +13,7 @@ userRouter.post("/login", validateBody(loginSchema), jsonParser, login);
 userRouter.post("/logout", authentificate, logout);
 userRouter.get("/current", authentificate, current);
 userRouter.patch("/avatar", authentificate, upload.single("avatar"), uploadAvatar);
+userRouter.get("/verify/:verifyToken", verifyEmail);
+userRouter.post("/verify", validateBody(emailSchema), resendVerifyEmail);
 
 export default userRouter;
